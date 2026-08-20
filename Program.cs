2af05d78676e14a9220f49255a0e031212e12b15
kartikakadu <kartikakadu13@gmail.com>
2026-08-20 08:16:06 +0530

    my first c# practice programs

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Console.WriteLine("Hello, World!");
+// Console.WriteLine("my name is kartika");
+// Console.WriteLine("i'm from pulgaon");
+// Console.WriteLine("my mother name is chanda");
+// Console.WriteLine("my father name is krushnarao");
+// Console.WriteLine("i completed my masters in computer science");
+
+// string name = "kartika";
+// int birthdate = 13; 
+// Console.WriteLine(name);
+// Console.WriteLine(birthdate);        
+
+// //******************************************************************************************
+// string name ="kartika";
+// int age =20;
+// string city ="pune";
+
+// Console.WriteLine("my name is"+name);
+// Console.WriteLine("my age is"+age);
+// Console.WriteLine("my city name is"+city);
+
+//************************************************************************************
+//simple calculator
+
+// int a = 25;
+// int b = 10;
+// int addition = a+b;
+// int subtraction =a-b;
+// int multiplication= a*b;
+// int division=a/b;
+// Console.WriteLine("addition" +addition);
+// Console.WriteLine("subtraction"+ subtraction);
+// Console.WriteLine("multiplication"+multiplication);
+// Console.WriteLine("division"+division);
+
+//*************************************************************************************
+//Square of given number
+
+// Console.Write("Enter the number");
+// int number= Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+// Console.WriteLine("the square of given number is " + square);
+
+//*************************************************************************************
+//Check whether the number is even or odd
+// Console.Write("Enter the number");
+// int num = Convert.ToInt32( Console.ReadLine());
+//  if(num%2==0)
+//  {
+//     Console.WriteLine("the given number is even number");
+//  }
+//  else
+//  {
+//     Console.WriteLine("the given number is a odd number");
+//  }
+
+//**************************************************************************************
+//Find the largest number
+Console.Write("Enter the number1");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the Number 2");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("the number is larger" +num1);
+}
+else if(num2> num1)
+{
+    Console.WriteLine("the number is larger" + num2);
+}
+else{
+    Console.WriteLine("Both numbers are equal");
+}
+
+
+
+
+
+
+
+
+
+
+
+
